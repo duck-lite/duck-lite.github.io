bf05f612d6fccb645d0cc0d43a435a9b208cb7a7
duck_lite <duck34364@gmail.com>
2024-07-02 16:07:24 +0800

    Site updated: 2024-07-02 16:07:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/02/test静态博客/","2024/07/02/hello/","2024/07/02/y总 的算法课/"];function toRandomPost(){
+var posts=["2024/07/02/y总 的算法课/","2024/07/02/test静态博客/","2024/07/02/hello/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
