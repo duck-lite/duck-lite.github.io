583bb865f211d2d7c2c55a452d1df3bb17fa7480
duck_lite <duck34364@gmail.com>
2024-07-10 08:58:58 +0800

    Site updated: 2024-07-10 08:58:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/02/y总 的算法课/","2024/07/08/静态的聊天机器人/","2024/07/08/蒟蒻的c-算法1-string类型/","2024/07/07/暑假Java学习路线/","2024/07/02/如何简单使用hexo框架下的静态博客/","2024/07/07/duck-lite的简陋Java文档/"];function toRandomPost(){
+var posts=["2024/07/07/duck-lite的简陋Java文档/","2024/07/02/如何简单使用hexo框架下的静态博客/","2024/07/08/蒟蒻的算法1--string类型/","2024/07/08/静态的聊天机器人/","2024/07/02/y总 的算法课/","2024/07/09/蒟蒻的数据结构/","2024/07/07/暑假Java学习路线/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
